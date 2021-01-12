37653a7e108cf5563cdb245938f0daa9189700ca
miladwaizi <milad.waizi@gmail.com>
2021-01-12 07:21:50 +0430

    sdf

--- Setting/AcademicYear.aspx.cs
+++ Setting/AcademicYear.aspx.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
 using System.Data.SqlClient;
 using System.Linq;
 using System.Web;
@@ -18,14 +20,29 @@ namespace BCS6thEMFall2020DemoPrject.Setting
 
         protected void btnSave_Click(object sender, EventArgs e)
         {
-            db.ExecuteDML(@"INSERT INTO [dbo].[tblSetup_AcademicYear]
-                            ([Title]
-                            ,[Abbreviation]
-                            ,[FarsiTitle]
-                            ,[InsertedBy]
-                            ,[InsertedDate])
-                             VALUES('"+txtTitle.Text+"','"+txtAbbreviation.Text+"',N'"+txtFarsiTitle.Text+"',1,'"+DateTime.Now.ToString()+"')");
-            ScriptManager.RegisterStartupScript(this, this.GetType(), "ss", "SuccessMsg();", true);
+           SqlConnection con = new SqlConnection(ConfigurationManager.ConnectionStrings["ConStr"].ConnectionString);
+            SqlCommand cmd = new SqlCommand("spPageAcademicYearSave", con);
+            cmd.CommandType = CommandType.StoredProcedure;
+            cmd.Parameters.AddWithValue("@Title", txtTitle.Text);
+            cmd.Parameters.AddWithValue("@Abbreviation", txtAbbreviation.Text);
+            SqlParameter p = new SqlParameter();
+            p.ParameterName = "@FarsiTitle";
+            p.Value = txtFarsiTitle.Text;
+            cmd.Parameters.Add(p);
+            cmd.Parameters.AddWithValue("@User", "1");
+            cmd.Parameters.AddWithValue("@Date", DateTime.Now.ToString());
+            cmd.Parameters.Add("@Error", SqlDbType.NVarChar);
+            cmd.Parameters["@Error"].Direction = ParameterDirection.Output;
+
+            con.Open();
+            cmd.ExecuteNonQuery();
+            string Error = (string) cmd.Parameters["@Error"].Value;
+            con.Close();
+
+            if (Error=="0")
+                ScriptManager.RegisterStartupScript(this, this.GetType(), "ss", "alert('Record Saved....!');", true);
+            else
+                ScriptManager.RegisterStartupScript(this, this.GetType(), "ss", "alert('"+Error+"');", true);
 
         }
     }
